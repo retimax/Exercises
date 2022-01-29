e0fa9d0b9291b489870504a7d3e1aeede2f0f3ac
Rolka <46093734+retimax@users.noreply.github.com>
2022-01-28 23:31:38 -0600

    Add files via upload

---
+++ JSFundaments/JS/constantes.js
@@ -0,0 +1,16 @@
+export const Pi = Math.PI;
+
+export let usuario = "Jon";
+
+const password = "qwerty";
+//export default password;
+
+export default function saludar() {
+  console.log("Hola modulos +ES6");
+}
+
+export class Saludar {
+    constructor() {
+        console.log("Hola clase +ES6")
+    }
+}
---
+++ JSFundaments/JS/modulos.js
@@ -0,0 +1,10 @@
+import saludar,{Pi, usuario, Saludar} from "./constantes.js"
+import {aritmetica as calculos} from "./aritmetica.js"
+console.log("Archivo modulos.js")
+
+console.log(Pi, usuario)
+
+console.log(calculos.sumar(3,3))
+saludar() 
+let saludo = new Saludar();
+saludo;
